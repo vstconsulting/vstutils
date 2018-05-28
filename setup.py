d80c2032b208942aaaad3797eacf5b531a58e124
Sergey Klyuykov <onegreyonewhite@mail.ru>
2018-05-28 11:21:03 +1000

    Refactoring and add rpc extra requires.
    For install with `rpc` deps use 'vstutils[rpc]' dep in requirements.txt

--- setup.py
+++ setup.py
@@ -4,12 +4,6 @@ from vstutils.compile import load_requirements, make_setup, os, find_packages
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
-RMF = os.path.join(os.path.dirname(__file__), 'README.rst')
-
-REQUIRES = load_requirements('requirements.txt')
-REQUIRES += load_requirements('requirements-doc.txt')
-REQUIRES_git = load_requirements('requirements-git.txt')
-
 ext_list = [
     'vstutils.environment',
     'vstutils.exceptions',
@@ -28,13 +22,16 @@ ext_list = [
 ]
 
 make_setup(
-    name='vstutils',
     packages=find_packages(exclude='tests'),
     ext_modules_list=ext_list,
     include_package_data=True,
     install_requires=[
         "django>=1.11,<2.0",
-    ] + REQUIRES,
+    ] +
+    load_requirements('requirements.txt') + load_requirements('requirements-doc.txt'),
+    extras_require={
+        'rpc': load_requirements('requirements-rpc.txt'),
+    },
     dependency_links=[
-    ] + REQUIRES_git,
+    ] + load_requirements('requirements-git.txt'),
 )
