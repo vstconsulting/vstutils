94b058b4fcace0e59e2b22681c8328b0f33a60d3
Sergey Klyuykov <onegreyonewhite@mail.ru>
2018-07-16 16:41:02 +1000

    Pylint disable `unused-argument`

--- vstutils/api/filter_backends.py
+++ vstutils/api/filter_backends.py
@@ -2,7 +2,9 @@
 class HideHiddenFilterBackend(object):
 
     def filter_queryset(self, request, queryset, view):
+        # pylint: disable=unused-argument
         return getattr(queryset, 'cleared', queryset.all)()
 
     def get_schema_fields(self, view):
+        # pylint: disable=unused-argument
         return []
