c4c2186a3552b4363881183c810f0961e4054dc7
psayker <kirill970528@yandex.ru>
2018-11-27 12:53:22 +1000

    Update documentation template

--- vstutils/doc_themes/vst-sphinx-theme/static/docs-sidebar.js
+++ vstutils/doc_themes/vst-sphinx-theme/static/docs-sidebar.js
@@ -5,42 +5,26 @@ $( document ).ready(function() {
     for(var i=0; i<headings.length; i++)
     {
         var t = headings[i].children[0].innerText;
-        $($('.toctree-l1')[i].children[0]).empty();
-        $($('.toctree-l1')[i].children[0]).prepend('<i class="fa fa-book doc-sidebar-icon"></i>');
-        $($('.toctree-l1')[i].children[0]).append('<span class="docs-heading-wrapper"><span class="docs-heading">' + t + '</span></span>');
-    }
+        $(headings[i].children[0]).empty();
+        $(headings[i].children[0]).prepend('<i class="nav-icon fa fa-book"></i>');
 
-    $(".sidebar-menu .toctree-l1 .reference.internal").mouseenter(function () {
-        var thisEl = this;
-            var docsHeadings = $(".sidebar-menu .docs-heading-wrapper");
-            var bool = false;
-            for(var i=0; i<docsHeadings.length; i++)
-            {
-                if($(docsHeadings[i]).is(':hover'))
-                {
-                    bool = true;
-                }
-            }
+        if(headings[i].children[1])
+        {
+           $(headings[i].children[0]).append('<p><span class="li-header-span "><i class=" li-header-span-i menu-text-data">' + t + '</i></span><i class="right fa fa-angle-left ico-menu"></i></p>');
+        }
+        else
+        {
+            $(headings[i].children[0]).append('<p><span class="li-header-span "><i class=" li-header-span-i menu-text-data">' + t + '</i></span></p>');
+        }
 
-            if(bool==false)
-            {
-                $(".sidebar-menu .docs-heading-wrapper-visible").removeClass("docs-heading-wrapper-visible");
-                $(thisEl).children('span').addClass('docs-heading-wrapper-visible');
-            }
-    })
+        $(headings[i].children[0]).addClass("nav-link");
+        $(headings[i]).addClass("nav-item");
 
-    $(".content-wrapper").hover(function ()
-    {
-        $(".sidebar-menu .docs-heading-wrapper-visible").removeClass("docs-heading-wrapper-visible");
-    })
+        if(headings[i].children[1])
+        {
+            let child_ul = headings[i].children[1];
 
-    $(".navbar").hover(function ()
-    {
-        $(".sidebar-menu .docs-heading-wrapper-visible").removeClass("docs-heading-wrapper-visible");
-    })
-
-    if(guiLocalSettings.get('hideMenu'))
-    {
-        $('body').addClass('sidebar-collapse');
+            $(child_ul).addClass("menu-treeview-menu nav nav-treeview");
+        }
     }
 });
