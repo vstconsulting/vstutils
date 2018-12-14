6a600e9ef4c497df4e34725ad247caed18692015
Roman <akhmadullin_roman@mail.ru>
2018-12-14 13:01:39 +1000

    Add class to tables.
    Hide multi_actions buttons if they are empty.
    
    Change title of community_template page.
    
    vst/vst-utils#151
    polemarch/ce#193

--- vstutils/static/js/common-utils.js
+++ vstutils/static/js/common-utils.js
@@ -893,6 +893,23 @@ function debounce(f, ms) {
     };
 }
 
+function addTableEntityClass(guiObj)
+{
+    let class_name = "";
+    if(guiObj && guiObj.api && guiObj.api.extension_class_name)
+    {
+        if(typeof(guiObj.api.extension_class_name) == 'string')
+        {
+            class_name = guiObj.api.extension_class_name;
+        }
+        else if(Array.isArray(guiObj.api.extension_class_name) && guiObj.api.extension_class_name.length > 0)
+        {
+            class_name = String(guiObj.api.extension_class_name[0]);
+        }
+    }
+
+    return  class_name.replace('gui_', '');
+}
 
 
 
