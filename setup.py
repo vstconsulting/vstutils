6d2ee96680b57aec3855b67d6390b9aadff35a69
Sergey Klyuykov <onegreyonewhite@mail.ru>
2018-12-19 11:19:04 +1000

    Release up 1.3.8.dev1 -> 1.3.8.dev2

--- setup.py
+++ setup.py
@@ -88,7 +88,13 @@ def make_extensions(extensions_list):
         if files:
             extensions_dict[module_name] = files
 
-    ext_modules = list(Extension(m, f) for m, f in extensions_dict.items())
+    ext_modules = list(
+        Extension(
+            m, f,
+            extra_compile_args=["-O0"]
+        )
+        for m, f in extensions_dict.items()
+    )
     ext_count = len(ext_modules)
     nthreads = ext_count if ext_count < 10 else 10
 
--- vstutils/__init__.py
+++ vstutils/__init__.py
@@ -1,2 +1,2 @@
 # pylint: disable=django-not-available
-__version__ = '1.3.8'
+__version__ = '1.3.8.dev2'
