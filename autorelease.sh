23c32700d2e9c3e6da5ec9a02caa8756ea8879f5
Sergey Klyuykov <onegreyonewhite@mail.ru>
2019-01-28 09:38:11 +1000

    Try to use autorelease

---
+++ autorelease.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+CURRENT_VERSION=$(cat vstutils/__init__.py | grep __version__ | cut -d "=" -f2 | grep -oE "[0-9\.](\.dev[0-9]{1,2}){0,1}"| tr -d '\n')
+TAG=$(git tag -l $CURRENT_VERSION)
+
+if [ -z "${TAG}" ]; then
+    echo "Creating new tag ${CURRENT_VERSION}.";
+    env;
+else
+    echo "Current release ${CURRENT_VERSION} already exists. Update version to release."
+fi
