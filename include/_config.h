6708879deebffe5dd1820657687c6b7e240a9d79
Sergey Klyuykov <onegreyonewhite@mail.ru>
2019-11-29 16:13:29 +1000

    Fix `warning:comparison between signed and unsigned integer expressions`

--- include/_config.h
+++ include/_config.h
@@ -2,7 +2,7 @@
 #include <ctype.h>
 
 int __has_only_whitespaces(char* s) {
-    for (int i = 0; i < strlen(s); i++) {
+    for (unsigned int i = 0; i < strlen(s); i++) {
         if (isspace(s[i]) == 0){
             return 0;
         }
