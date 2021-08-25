8e4c103cab77ebab015496a7098381aa2deba435
Michael Gusev <gusev-miha2011@mail.ru>
2021-08-25 12:10:17 +1000

    Fix: router back and svg validation

---
+++ frontend_src/vstutils/fields/files/__tests__/readfiles.test.js
@@ -0,0 +1,28 @@
+import { expect, test, describe, jest } from '@jest/globals';
+import ResolutionValidatorMixin from '../named-binary-image/ResolutionValidatorMixin';
+import { NamedBinaryImageField } from '../named-binary-image';
+
+describe('File fields', () => {
+    test('Call read files', async () => {
+        const field = new NamedBinaryImageField({
+            name: 'field',
+            additionalProperties: {
+                extensions: ['jpg'],
+            },
+        });
+
+        const validatedCallback = jest.fn();
+
+        await ResolutionValidatorMixin.methods.readFiles.call(
+            {
+                $parent: { field },
+                field,
+                onImageValidated: validatedCallback,
+            },
+            [],
+        );
+
+        expect(validatedCallback).toBeCalledTimes(1);
+        expect(validatedCallback.mock.calls[0][0].length).toBe(0);
+    });
+});
