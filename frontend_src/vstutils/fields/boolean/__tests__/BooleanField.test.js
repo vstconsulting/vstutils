d7beea014f32e0b772bbe908d8f25d4bd6c15a71
Ivan Krisko <vanya9.9@mail.ru>
2021-08-31 16:20:58 +1000

    FIX: Page update when path changed

---
+++ frontend_src/vstutils/fields/boolean/__tests__/BooleanField.test.js
@@ -0,0 +1,16 @@
+import { expect, test, describe } from '@jest/globals';
+import { BooleanField } from '../../boolean';
+
+describe('BooleanField', () => {
+    test('test boolean toInner', () => {
+        const booleanField = new BooleanField({
+            name: 'boolean',
+        });
+        expect(booleanField.toInner({ boolean: 'true' })).toBeTruthy();
+        expect(booleanField.toInner({ boolean: true })).toBeTruthy();
+        expect(booleanField.toInner({ boolean: 1 })).toBeTruthy();
+        expect(booleanField.toInner({ boolean: 'false' })).toBeFalsy();
+        expect(booleanField.toInner({ boolean: false })).toBeFalsy();
+        expect(booleanField.toInner({ boolean: 0 })).toBeFalsy();
+    });
+});
