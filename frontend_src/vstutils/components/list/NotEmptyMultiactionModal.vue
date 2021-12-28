3d92287c684eaa990cf87490342258a4ef6b5827
Ivan Krisko <vanya9.9@mail.ru>
2021-12-28 18:24:28 +1000

    Fix: Multiactions with error

--- frontend_src/vstutils/components/list/NotEmptyMultiactionModal.vue
+++ frontend_src/vstutils/components/list/NotEmptyMultiactionModal.vue
@@ -63,40 +63,39 @@
                     this.$app.error_handler.defineErrorAndShow(e);
                     return;
                 }
-                try {
-                    const method = this.view.method;
-                    const headers = { 'content-type': 'application/json' };
-                    const responses = await Promise.all(
-                        this.getRequestPaths().map((path) =>
-                            this.$app.api.makeRequest({
-                                method,
-                                headers,
-                                path,
-                                data: instance._getInnerData(),
-                                useBulk: instance.constructor.shouldUseBulk(method),
-                            }),
-                        ),
-                    );
-                    this.changedFields = [];
-                    for (const response of responses) {
+                const method = this.view.method;
+                const headers = { 'content-type': 'application/json' };
+                const responses = await Promise.allSettled(
+                    this.getRequestPaths().map((path) =>
+                        this.$app.api.makeRequest({
+                            method,
+                            headers,
+                            path,
+                            data: instance._getInnerData(),
+                            useBulk: instance.constructor.shouldUseBulk(method),
+                        }),
+                    ),
+                );
+                this.changedFields = [];
+                for (const response of responses) {
+                    if (response.status === 'fulfilled') {
                         guiPopUp.success(
                             this.$t(pop_up_msg.instance.success.execute, [
                                 this.$t(this.view.title),
-                                parseResponseMessage(response.data),
+                                parseResponseMessage(response.value.data),
                             ]),
                         );
+                    } else {
+                        const str = this.$app.error_handler.errorToString(response.reason);
+                        const srt_to_show = this.$t(pop_up_msg.instance.error.execute, [
+                            this.$t(this.view.title),
+                            str,
+                        ]);
+                        this.$app.error_handler.showError(srt_to_show, str);
                     }
-                    this.pageComponent.fetchData();
-                } catch (error) {
-                    const str = this.$app.error_handler.errorToString(error);
-                    const srt_to_show = this.$t(pop_up_msg.instance.error.execute, [
-                        this.$t(this.view.title),
-                        str,
-                    ]);
-                    this.$app.error_handler.showError(srt_to_show, str);
-                } finally {
-                    this.$refs.modal.close();
                 }
+                this.pageComponent.fetchData();
+                this.$refs.modal.close();
             },
         },
     };
