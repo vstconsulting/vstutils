1b7b096855fbc91444821310c46020567e85bdaa
Sergey Kulukov <onegreyonewhite@mail.ru>
2022-01-27 17:44:54 +1000

    Fix: babel transform and update dependencies.

--- frontend_src/.babelrc.js
+++ frontend_src/.babelrc.js
@@ -1,7 +1,21 @@
 module.exports = {
-    presets: ['@babel/preset-env'],
+    presets: [
+        [
+            '@babel/preset-env',
+            {
+                useBuiltIns: 'usage',
+                corejs: { version: '3.20.3', proposals: true },
+            },
+        ],
+    ],
     plugins: [
-        '@babel/plugin-transform-runtime',
-        '@babel/plugin-proposal-class-properties',
+        [
+            '@babel/plugin-transform-runtime',
+            {
+                'corejs': false,
+                'regenerator': false,
+                'version': '^7.16.8'
+            }
+        ],
     ],
 };
