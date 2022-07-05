12f7927a0b417e97ef480075060aba1a75dab78e
Sergey Kulukov <onegreyonewhite@mail.ru>
2022-07-05 15:52:15 +1000

    Fix: CVE-2022-34265

--- setup.py
+++ setup.py
@@ -399,7 +399,7 @@ kwargs = dict(
         'vstutils/static/bundle/.*\.js$'
     ],
     install_requires=[
-        "django~=" + (os.environ.get('DJANGO_DEP', "") or "3.2.13"),
+        "django~=" + (os.environ.get('DJANGO_DEP', "") or "3.2.14"),
     ]
     + requirements
     + load_requirements('requirements-doc.txt'),
