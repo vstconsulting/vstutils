5416b71a1d3bb37997e9ec9b444f9894267cfc13
Dmitriy Ovcharenko <ovcharenkodd@gmail.com>
2023-07-13 11:31:36 +1000

    Feature(frontend): Add file selector

--- frontend_src/vstutils/fields/text/TextAreaFieldContentEdit.vue
+++ frontend_src/vstutils/fields/text/TextAreaFieldContentEdit.vue
@@ -1,23 +1,50 @@
 <template>
-    <FieldTextarea :value="value || ''" :field="field" @input="$emit('set-value', $event)" />
+    <div>
+        <FileInputButton style="margin-bottom: 0.5rem" :accept="accept" @input.native="handleFileInput" />
+        <FieldTextarea :value="value || ''" :field="field" @input="$emit('set-value', $event)" />
+    </div>
 </template>
 
 <script lang="ts">
-    import { defineComponent } from 'vue';
+    import { computed, defineComponent } from 'vue';
     import {
         FieldEditPropsDef,
         FieldEditEmitsDef,
         type FieldEditEmitsDefType,
         type FieldEditPropsDefType,
     } from '@/vstutils/fields/base';
+    import FileInputButton from '@/vstutils/components/FileInputButton.vue';
     import FieldTextarea from './FieldTextarea.vue';
     import type { TextAreaField } from './TextAreaField';
+    import type { IFileField } from '../files/file/utils';
 
     export default defineComponent({
         components: {
             FieldTextarea,
+            FileInputButton,
         },
         props: FieldEditPropsDef as FieldEditPropsDefType<TextAreaField>,
         emits: FieldEditEmitsDef as FieldEditEmitsDefType<TextAreaField>,
+        setup(props, { emit }) {
+            const accept = computed(() => {
+                if ('allowedMediaTypes' in props.field) {
+                    const field = props.field as IFileField;
+                    return field.allowedMediaTypes?.join(',');
+                }
+                return undefined;
+            });
+
+            async function handleFileInput(e: Event) {
+                const input = e.target as HTMLInputElement;
+                const file = (input.files ?? [])[0];
+                if (file) {
+                    const text = await file.text();
+                    emit('set-value', text);
+                    input.value = '';
+                }
+            }
+
+            return { accept, handleFileInput };
+        },
     });
 </script>
